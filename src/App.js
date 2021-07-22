3dd6d7bbc8cd1b6c054124788771ac285c95385d
devbryan0225 <devbryan0225@users.noreply.github.com>
2021-07-22 22:53:08 +0800

    Calling values.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+const title = 'React changed';
+
+const welcome = {
+  greeting: 'hey',
+  title: 'React'
+};
+
+function getTitle(title) {
+  return title;
+}
 
 function App() {
   return (
     <div>
-     <h1>Hello World</h1>
+     <h1>Hello {title}</h1>
+     <h2>{welcome.greeting} {welcome.title}</h2>
+     <h3>Hello {getTitle("React")}</h3>
+     <label htmlFor="search">Search: </label>
+     <input id="search" type="text"/>
     </div>
   );
 }
