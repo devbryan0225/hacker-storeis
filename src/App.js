77749e5718c6eb3e179dfa5ea2650b1c7dec6e86
devbryan0225 <devbryan0225@users.noreply.github.com>
2021-07-25 21:59:37 +0800

    Iterate list.

--- src/App.js
+++ src/App.js
@@ -9,6 +9,25 @@ function getTitle(title) {
   return title;
 }
 
+const list = [
+{
+  title: 'React',
+  url: 'https://reactjs.org/',
+  author: 'Jordan Walke',
+  num_comments: 3,
+  points: 4,
+  objectID: 0,
+},
+{
+  title: 'Redux',
+  url: 'https://redux.js.org/',
+  author: 'Dan Abramov, Andrew Clark',
+  num_comments: 2,
+  points: 5,
+  objectID: 1,
+}
+]; 
+
 function App() {
   return (
     <div>
@@ -17,6 +36,27 @@ function App() {
      <h3>Hello {getTitle("React")}</h3>
      <label htmlFor="search">Search: </label>
      <input id="search" type="text"/>
+     <ul>
+       {list.map(function(item) {
+         return (
+          <li key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+          
+          
+          </li>
+          
+          );
+       })}
+     </ul>
+     <hr/>
+
+     {/*this is how you comment.*/}
+     
     </div>
   );
 }
